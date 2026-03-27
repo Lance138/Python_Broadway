61aec1946f414099a2130512887b75b753a1a019
Priyesh Shrestha <priyeshshrestha@Priyeshs-MacBook-Air.local>
2026-03-27 08:55:04 +0545

    first commit

---
+++ basics.py
@@ -0,0 +1,25 @@
+print("Hello from Broadway!")
+#good
+data = "Hello"
+data = 1.025
+DATA = "Hello world"
+
+dasda = 1
+FAda = 12
+data1 = 144
+rest_take = 1
+_test = 123
+
+#bad
+1test = 1
+!test$ = 2
+print = 4
+
+#datatypes
+a = "1" #string
+a = 1 #integer
+a = 1.4 #float
+a = True #Bool
+a = None #None type
+a = "" #This is string
+
